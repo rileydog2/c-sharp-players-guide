77b25befbd6121402692a176e048589ad78e4ec7
RileyDog <148552625+rileydog2@users.noreply.github.com>
2026-04-03 09:24:00 -0400

    Auto stash before rebase of "main"

---
+++ MakingsOfAProgrammer/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("I'm learning C# fundamentals.");
+Console.WriteLine("Next come ASP.NET Core");
+Console.WriteLine("Not real sure what that is all about yet");
+Console.WriteLine("I'm sure I'll figure it out");
+Console.WriteLine("Looking for a new job by Christmas!");
+
+// I think there might be a limit to size of a program
+// based on hardware constraints. I'm sure that is probably
+// a huge number though and not like to be an issue.
